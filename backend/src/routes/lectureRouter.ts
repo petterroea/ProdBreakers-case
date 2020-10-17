5fc9858d6d23bc8d884553002c8c791f7464b807
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2020-10-17 10:53:29 +0200

    Add PUT request for lectures

--- backend/src/routes/lectureRouter.ts
+++ backend/src/routes/lectureRouter.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { getLectureRepository } from '../database';
 import { User } from '../entities/user';
 import { Lecture } from '../entities/lecture';
+import userMiddleware from '../middlewares/userMiddleware';
 
 const lectureRouter = Router();
 
@@ -13,8 +14,9 @@ const censorUserMap = (user: User) => {
 
 
 lectureRouter.get('/', async (req, res) => {
-  const lectures = (await getLectureRepository().find()).map((lecture) => {
-  	lecture.owner = censorUserMap(lecture.owner)
+  const lectures = (await getLectureRepository().find());
+  lectures.forEach((lecture) => {
+    lecture.owner = censorUserMap(lecture.owner)
   });
 
   res.json(lectures);
@@ -26,6 +28,31 @@ lectureRouter.get('/:uuid', async (req, res) => {
   }});
   lecture.owner.hashedPassword = undefined;
   res.json(lecture);
+});
+
+lectureRouter.use(userMiddleware);
+
+lectureRouter.put('/', async (req, res) => {
+  const body: Lecture = req.body;
+  try {
+    const owner: User = req.user;
+    const {
+      name,
+      description,
+      start,
+      end,
+    } = body;
+    await getLectureRepository().save({
+      owner,
+      name,
+      description,
+      start,
+      end
+    });
+    res.sendStatus(200);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
 export default lectureRouter;
