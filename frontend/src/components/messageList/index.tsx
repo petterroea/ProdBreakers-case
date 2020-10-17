a335e228fa732a27d70113b0aa9206ca8c0750e3
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2020-10-17 22:55:07 +0200

    Add dont look for comments on undefined lectures

--- frontend/src/components/messageList/index.tsx
+++ frontend/src/components/messageList/index.tsx
@@ -5,30 +5,30 @@ import Thread from './Thread';
 import { RootStateType } from '../../state/reducers';
 
 const MessageList = (props: any) => {
-  const currentLecture: string = props.currentLecture;
-  const [messages, setMessages] = useState(null as any[]|null);
+    const currentLecture: string = props.currentLecture;
+    const [messages, setMessages] = useState(null as any[] | null);
 
-  const authSelector = useSelector((state: RootStateType) => state.authentication);
-  const token: string|undefined = authSelector.user?.token;
+    const authSelector = useSelector((state: RootStateType) => state.authentication);
+    const token: string | undefined = authSelector.user?.token;
 
-  useEffect(() => {
-    const internal = async () => {
-      const res: any = await fetch(`/api/comment/lecture/${currentLecture}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        }, // technically not necessary right now, but we might change that
-      });
-      const msgs = await res.json();
-      setMessages(msgs);
-    };
-    internal();
-  }, [currentLecture, token]);
+    useEffect(() => {
+        const internal = async () => {
+            const res: any = await fetch(`/api/comment/lecture/${currentLecture}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }, // technically not necessary right now, but we might change that
+            });
+            const msgs = await res.json();
+            setMessages(msgs);
+        };
+        if (currentLecture) {
+            internal();
+        }
+    }, [currentLecture, token]);
 
-  return (
-    <div>
-      {messages?.map((message: any) => <Thread key={message.uuid} message={message} />) ?? 'Loading...'}
-    </div>
-  );
-}
+    return (
+        <div>{messages?.map((message: any) => <Thread key={message.uuid} message={message} />) ?? 'Loading...'}</div>
+    );
+};
 
 export default MessageList;
