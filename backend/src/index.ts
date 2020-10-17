682393c16deebeaa1d83ee010b69735aee2d77d1
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2020-10-17 09:29:38 +0200

    Add semicolons and a user middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,12 @@ import cors from 'cors';
 import createError from 'http-errors';
 import http from 'http';
 
-import { setupDatabaseConection } from './database'
+import { setupDatabaseConnection } from './database';
 
 import userRouter from './routes/userRouter';
 import lectureRouter from './routes/lectureRouter';
 
-import { dbConfig } from './config'
+import { dbConfig } from './config';
 
 
 const port = process.env.PORT || '9000';
@@ -40,7 +40,7 @@ const setupApp = (): express.Application => {
 }
 
 
-setupDatabaseConection(dbConfig).then(() => {
+setupDatabaseConnection(dbConfig).then(() => {
 	const server = http.createServer(setupApp());
 
 	server.listen(port);
