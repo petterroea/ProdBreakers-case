2641483d671cc67f76487e1208c560aa5667c95c
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2020-10-18 11:04:24 +0200

    Type declaration

--- frontend/src/components/messageList/index.tsx
+++ frontend/src/components/messageList/index.tsx
@@ -6,21 +6,23 @@ import { RootStateType } from '../../state/reducers';
 
 const MessageList = (props: any) => {
     const currentLecture: string = props.lecture;
-    const [messages, setMessages] = useState(null as any[] | null);
+    const [messages, setMessages] = useState<Array<any>>([]);
+    const [loading, setLoading] = useState(false);
 
     const authSelector = useSelector((state: RootStateType) => state.authentication);
     const token: string | undefined = authSelector.user?.token;
 
     useEffect(() => {
         const internal = async () => {
+            setLoading(true);
             const res: any = await fetch(`/api/comment/lecture/${currentLecture}`, {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 }, // technically not necessary right now, but we might change that
             });
             const msgs = await res.json();
-            console.log(msgs);
             setMessages(msgs);
+            setLoading(false);
         };
         if (currentLecture) {
             internal();
